7daad8c42eae92610b90d2d71592ab2fe2b3b7a2
Nick <144842765+Nexexexexed@users.noreply.github.com>
2025-05-24 21:52:21 +0300

    deploy

--- .eslintrc.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: { browser: true, es2023: true },
-  extends: ["eslint:recommended"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  rules: { "no-unused-vars": ["warn"] },
-};
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import "./styles/main.css";
-import "./components/ruler-scale.js";
-import "./components/app-root.js";
-import "./components/buffer-zone.js";
-import "./components/workspace-canvas.js";
-import "./components/polygon-item.js";
